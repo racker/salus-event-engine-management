8f2e87084baa685c1c0d2fa69c1bf6dd848c49b2
Adam <harageth@gmail.com>
2019-05-02 09:24:13 -0700

    added more replacement parameters in the pom and fixed the path in TasksApi

--- src/main/java/com/rackspace/salus/event/manage/web/TasksApi.java
+++ src/main/java/com/rackspace/salus/event/manage/web/TasksApi.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/tasks/tenant")
+@RequestMapping("/api/tasks/")
 @Api(description = "Monitor operations", authorizations = {
         @Authorization(value = "repose_auth",
                 scopes = {
