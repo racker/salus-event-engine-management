cf22bd8ceb1e49aed45509ba1e2f115ef0f6961a
James Buchan <james.buchan@rackspace.com>
2020-10-21 10:05:08 -0600

    Fix tests

--- src/main/java/com/rackspace/salus/event/manage/web/model/EventEngineTaskDTO.java
+++ src/main/java/com/rackspace/salus/event/manage/web/model/EventEngineTaskDTO.java
@@ -17,15 +17,26 @@
 package com.rackspace.salus.event.manage.web.model;
 
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonView;
+import com.rackspace.monplat.protocol.UniversalMetricFrame;
+import com.rackspace.monplat.protocol.UniversalMetricFrame.MonitoringSystem;
+import com.rackspace.salus.common.web.View;
+import com.rackspace.salus.event.manage.model.ValidationGroups.Create;
+import com.rackspace.salus.event.manage.model.ValidationGroups.Test;
 import com.rackspace.salus.telemetry.entities.EventEngineTask;
 import com.rackspace.salus.telemetry.entities.EventEngineTaskParameters;
-import com.rackspace.salus.common.web.View;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+import javax.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "monitoringSystem", defaultImpl = GenericEventEngineTaskDTO.class)
+@JsonSubTypes({
+    @JsonSubTypes.Type(name = "salus", value = SalusEventEngineTaskDTO.class)
+})
 @Data @NoArgsConstructor
 public abstract class EventEngineTaskDTO {
   UUID id;
@@ -38,9 +49,12 @@ public abstract class EventEngineTaskDTO {
   EventEngineTaskParameters taskParameters;
   String createdTimestamp;
   String updatedTimestamp;
+  @NotNull(groups = {Create.class, Test.class})
+  UniversalMetricFrame.MonitoringSystem monitoringSystem;
 
   public EventEngineTaskDTO(EventEngineTask entity) {
     this.id = entity.getId();
+    this.monitoringSystem = MonitoringSystem.valueOf(entity.getMonitoringSystem());
     this.tenantId = entity.getTenantId();
     this.name = entity.getName();
     this.taskParameters = entity.getTaskParameters();
